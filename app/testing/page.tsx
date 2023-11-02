28a2de1ae0a6d841f9362ce4ec6ae15507b60c6f
muhammadhanief <haniefm19@gmail.com>
2023-11-02 21:08:45 +0700

    ketiga

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import Pertama from "@/public/skrips/pertama.jpeg";
+import Ketiga from "@/public/skrips/ketiga.jpeg";
 
 const page = () => {
   return (
     <div>
       <Image src={Pertama} alt="" width={200} />
+      <Image src={Ketiga} alt="" width={200} />
     </div>
   );
 };
